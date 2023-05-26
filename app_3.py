afef47fa9ac913806c8dc59991a0f4ec64aad8d0
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-05-26 17:25:09 +0530

    works

--- app_3.py
+++ app_3.py
@@ -1,52 +1,28 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk import ne_chunk, pos_tag
-
+import spacy
 from flask import Flask, request, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk import ne_chunk
+import spacy.cli
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+#spacy.cli.download("en_core_web_lg")
 
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+nlp1 = spacy.load("./model1/content/output/model-best") #load the best model
+
 def generate_bot_response(user_message):
-
-    print("In generate bot response")
-
-    print(user_message)
-
-    if user_message is None:
-        return "I'm sorry, I couldn't understand your message. How can I assist you?"
-
-    tokens = word_tokenize(user_message.lower())
-
-
-    stop_words = set(stopwords.words('english'))
-    print(user_message)
-    tokens = word_tokenize(user_message.lower())
-    stop_words = set(stopwords.words('english'))
-    filtered_tokens = [token for token in tokens if token not in stop_words]
-
-    ner_tags = ne_chunk(pos_tag(filtered_tokens))
-
-    for chunk in ner_tags:
-        if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
-            recipient = ' '.join(c[0] for c in chunk.leaves())
-            message_content = extract_message_content(user_message)
-            if recipient and message_content:
-                return send_message(recipient, message_content)
-
+    doc = nlp1(user_message) # input sample text
+    # Find named entities, phrases and concepts
+    for entity in doc.ents:
+        print(entity.text, entity.label_)
+        if entity.label_== "RECIPIENT_PHONE":
+            recipient_number = entity.text
+        if entity.label_== "MESSAGE":
+            message = entity.text
+    send_message_return = send_message(recipient_number, message)
+    print(send_message_return)
     return "I'm sorry, I couldn't understand your message. How can I assist you?"
 
 def send_message(recipient, message):
@@ -79,19 +55,6 @@ def send_message(recipient, message):
         return f"Failed to send message: {str(e)}"
 
     return 
-
-def extract_message_content(user_message):
-    # Extract the content of the message between "message:" and "to:" (if present)
-
-    print("In extract messge content")
-    start_index = user_message.find("message:")
-    end_index = user_message.find("to:")
-    if start_index != -1 and end_index != -1:
-        content = user_message[start_index + len("message:"):end_index].strip()
-        return content
-    else:
-        return ""
-
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
