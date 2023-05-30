26fe73a6cc4dbedeca9f8df5cafa8bd5412f38b4
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-05-30 11:21:55 +0530

    upadte ui

--- chatbot-frontend/src/App.js
+++ chatbot-frontend/src/App.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
-import cors from "cors";
-
-// const corsOptions = {
-//   origin: "http://127.0.0.1:5000/sms", // Update this with the actual backend URL
-// };
+import "./App.css";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [botResponse, setBotResponse] = useState("");
 
   const sendMessage = async () => {
+    // Send the user's message to the backend API
     const response = await fetch("http://127.0.0.1:5000/sms", {
       method: "POST",
       headers: {
@@ -20,67 +17,39 @@ function App() {
 
     const data = await response.json();
 
-    setBotResponse(data);
-
-    // Log the bot response in the console
-    console.log(data);
+    // Update the bot's response in the state
+    setBotResponse(data.Body);
   };
 
   return (
-    <div>
-      <h1>AI Chatbot</h1>
-      <div>
+    <div className="chat-container">
+      <div className="chat-header">
+        <img
+          className="chatbot-image"
+          src="path/to/your/chatbot-image.png"
+          alt="Chatbot"
+        />
+        <h1>Twilio AI Messenger</h1>
+      </div>
+      <div className="chat-body">
+        <div className="message-container user-message">
+          <p>{userInput}</p>
+        </div>
+        <div className="message-container bot-message">
+          <p>{botResponse}</p>
+        </div>
+      </div>
+      <div className="chat-input">
         <input
           type="text"
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
+          placeholder="Type your message here..."
         />
         <button onClick={sendMessage}>Send</button>
       </div>
-      <p>{botResponse}</p>
     </div>
   );
 }
 
-// export default cors(corsOptions)(App);
 export default App;
-
-// import React, { useState } from "react";
-
-// function App() {
-//   const [userInput, setUserInput] = useState("");
-//   const [botResponse, setBotResponse] = useState("");
-
-//   const sendMessage = async () => {
-//     // Send the user's message to the backend API
-//     const response = await fetch("http://127.0.0.1:5000/sms", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ Body: userInput }),
-//     });
-
-//     const data = await response.json();
-
-//     // Update the bot's response in the state
-//     setBotResponse(data.Body);
-//   };
-
-//   return (
-//     <div>
-//       <h1>AI Chatbot</h1>
-//       <div>
-//         <input
-//           type="text"
-//           value={userInput}
-//           onChange={(e) => setUserInput(e.target.value)}
-//         />
-//         <button onClick={sendMessage}>Send</button>
-//       </div>
-//       <p>{botResponse}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
